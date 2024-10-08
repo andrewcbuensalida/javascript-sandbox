98fc580b7b36820238f0e0d81c1d8e39acb3cc8f
Andrew Buensalida <andrewcbuensalida@gmail.com>
2024-10-08 12:22:03 -0700

    settimeout

---
+++ curlyBrackets.js
@@ -0,0 +1,5 @@
+let x = ('a','b')
+typeof x
+console.log(`Example typeof x: `, typeof x)
+x
+console.log(`Example x: `, x)
---
+++ setTimeout.js
@@ -0,0 +1,12 @@
+function setTimeoutTest() {
+	console.log("setTimeoutTest0");
+	setTimeout(() => {
+		console.log("setTimeoutTest1");
+	}, 5000);
+	setTimeout(() => {
+		console.log("setTimeoutTest2");
+	}, 5000);
+	console.log("setTimeoutTest3");
+}
+
+setTimeoutTest();
